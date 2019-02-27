517c2550b9aaf20fd32b278fc423f23dc6bebe46
zzhuangqian <18701626636@163.com>
2019-02-27 13:53:30 +0800

    添加首页内容区域

---
+++ components/index/life.vue
@@ -0,0 +1,42 @@
+<template>
+  <div class="m-life">
+    <el-row>
+      <el-col :span="14">
+        <Slider/>
+      </el-col>
+      <el-col :span="4"></el-col>
+      <el-col :span="6">
+        <div class="m-life-login">
+          <h4>
+            <img src="//s0.meituan.net/bs/fe-web-meituan/2d05c2b/img/avatar.jpg" alt="">
+          </h4>
+          <p class="m-life-login-name">Hi 你好</p>
+          <p><nuxt-link to="/register">
+            <el-button round size="medium">注册 </el-button>
+          </nuxt-link></p>
+          <p>
+            <nuxt-link to="/login">
+              <el-button round size="medium">立即登陆</el-button>
+            </nuxt-link>
+          </p>
+        </div>
+
+
+      </el-col>
+
+    </el-row>
+    <el-row></el-row>
+  </div>
+
+</template>
+<script>
+import Slider from './slider.vue'
+export default {
+  components: {
+    Slider
+  }
+}
+</script>
+<style lang="scss">
+@import '@/assets/css/index/life.scss';
+</style>
---
+++ pages/register.vue
@@ -0,0 +1,20 @@
+<template>
+    <u-page>
+
+    </u-page>
+
+</template>
+
+<script type="text/ecmascript-6">
+  import UPage from 'common/components/u-page'
+
+  export default {
+    components: {
+      UPage
+    }
+  }
+</script>
+
+<style lang="stylus" rel="stylesheet/stylus">
+
+</style>
